f64bc5d1e6c4bacdaeb7f63390b843116f58c702
Jesse Yang <jesseyang432@gmail.com>
2022-11-22 14:42:53 -0600

    Added home page which currently lists all courses

---
+++ client/components/Course/CourseComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    <article
+      class="course"
+    >
+      <header>
+        <h3 class="name">
+          {{ course.name }}
+        </h3>
+      </header>
+      <p class="description">
+        {{course.description}}
+      </p>
+      <section class="alerts">
+        <article
+          v-for="(status, alert, index) in alerts"
+          :key="index"
+          :class="status"
+        >
+          <p>{{ alert }}</p>
+        </article>
+      </section>
+    </article>
+  </template>
+  
+  <script>
+  export default {
+    name: 'CourseComponent',
+    props: {
+      // Data from the stored course
+      course: {
+        type: Object,
+        required: true
+      }
+    },
+    data() {
+      return {
+        alerts: {} // Displays success/error messages encountered during course modification
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .course {
+      border: 1px solid #111;
+      border-radius: 16px;
+      margin: 16px;
+      padding: 20px;
+      position: relative;
+  }
+  </style>
+  
