e6d7356bf7b8ffa178cf565550e21f82289b30e5
Johan Aguilar <aguilarfile7@gmail.com>
2022-11-29 22:52:37 -0500

    Fixed the styling removal

--- client/components/common/BlockForm.vue
+++ client/components/common/BlockForm.vue
@@ -11,27 +11,29 @@
         v-for="field in fields"
         :key="field.id"
       >
-        <label :for="field.id">{{ field.label }}:</label>
         <textarea
           v-if="field.id === 'content'"
           :name="field.id"
           :value="field.value"
+          placeholder= "Compose your message..."
           @input="field.value = $event.target.value"
         />
-        <select v-else-if="field.id === 'classYear'"
+        <select required v-else-if="field.id === 'classYear'"
           :name="field.id"
           :value="field.value"
           @input="field.value = $event.target.value">
+          <option value="" disabled selected hidden>{{ field.label }}</option>
           <option value="2023">2023</option>
           <option value="2024">2024</option>
           <option value="2025">2025</option>
           <option value="2026">2026</option>ß
         </select>
       
-        <select v-else-if="field.id === 'major'"
+        <select required v-else-if="field.id === 'major'"
           :name="field.id"
           :value="field.value"
           @input="field.value = $event.target.value">
+          <option value="" disabled selected hidden>{{ field.label }}</option>
           <option value="6-3">6-3 Computer Science</option>
           <option value="6-2">6-2 EE and Computer Science</option>
           <option value="17">17 Basket Weaving</option>
@@ -39,10 +41,11 @@
           <option value="29">29 Bicycle Making</option>
         </select>
 
-        <select v-else-if="field.id === 'term'"
+        <select required v-else-if="field.id === 'term'"
           :name="field.id"
           :value="field.value"
           @input="field.value = $event.target.value">
+          <option value="" disabled selected hidden>{{ field.label }}</option>
           <option value="S2023">Spring 2023</option>
           <option value="F2022">Fall 2022</option>
           <option value="S2022">Spring 2022</option>
@@ -50,12 +53,29 @@
           <option value="S2021">Spring 2021</option>
           <option value="F2020">Fall 2020</option>
         </select>
-
-        <input
-          v-else
-          :type="field.id === 'password' ? 'password' : 'text'"
+        <select required v-else-if="field.id === 'course'"
           :name="field.id"
           :value="field.value"
+          @input="field.value = $event.target.value">
+          <option value="" disabled selected hidden>{{ field.label }}</option>
+          <option
+            v-for="course in $store.state.enrollments"
+            :key="course.toCourse.id"
+            :value=course.toCourse.name
+          >
+            {{course.toCourse.name}}
+          </option>
+        </select>
+        <input v-else-if = "field.id === 'password'"
+          :name="field.id"
+          :value="field.value"
+          placeholder="Password"
+          @input="field.value = $event.target.value"
+        >
+        <input v-else-if = "field.id === 'username'"
+          :name="field.id"
+          :value="field.value"
+          placeholder="Username"
           @input="field.value = $event.target.value"
         >
       </div>
@@ -63,11 +83,11 @@
     <article v-else>
       <p>{{ content }}</p>
     </article>
-    <button
-      type="submit"
-    >
-      {{ title }}
-    </button>
+    <p style="text-align: right;">
+      <button type="submit">
+        {{ title }}
+      </button>
+    </p>
     <section class="alerts">
       <article
         v-for="(status, alert, index) in alerts"
@@ -155,36 +175,74 @@ export default {
 
 <style scoped>
 form {
-  border: 1px solid #111;
-  padding: 0.5rem;
+  border: 1px solid #A9A9A9;
+  border-radius: 16px;
+  padding: 10px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   margin-bottom: 14px;
   position: relative;
-  background-color: white;
 }
-
 article > div {
   display: flex;
   flex-direction: column;
 }
-
 form > article p {
   margin: 0;
 }
-
 form h3,
 form > * {
   margin: 0.3em 0;
+  font-family: 'Inter';
+  font-weight: normal;
 }
-
 form h3 {
   margin-top: 0;
+  
 }
-
+select {
+  border: 0px;
+  border-radius: 10px;
+  padding: 8px 12px;
+  font-size: .8em;
+  background-color: #F6F6F6;
+  margin-top: 10px;
+}
+select:invalid {
+  color: #747574;
+}
 textarea {
-   font-family: inherit;
-   font-size: inherit;
+  font-family: inherit;
+  font-size: inherit;
+  border: 0px;
+  border-radius: 20px;
+  padding: 8px 12px;
+  padding-bottom: 70px;
+  font-size: .8em;
+  background-color: #F6F6F6;
+  margin-top: 10px;
+}
+textarea::placeholder {
+  font-family: "Inter";
+}
+button {
+  border: 0px;
+  border-radius: 10px;
+  padding: 8px 12px;
+  font-size: .8em;
+  background-color: #288BFF;
+  margin-top: 0px;
+  color: white;
+  block-size: fit-content;
+  width: fit-content;
+}
+input {
+  border: 0px;
+  border-radius: 10px;
+  padding: 8px 12px;
+  font-size: .8em;
+  background-color: #F6F6F6;
+  margin-top: 10px;
 }
 </style>
